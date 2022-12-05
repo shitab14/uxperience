47f5e23b5fa83a56d6b85390ad5bb35386b73af7
Shitab Mir <mushfiq.islam@hungrynaki.com>
2022-12-05 15:35:26 +0600

    Init Commit

---
+++ app/src/main/java/smir/shitab14/uxperience/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package smir.shitab14.uxperience.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import smir.shitab14.uxperience.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
