fdfb56b9f09650b9c88f5f1130e4616397f97df9
Shitab Mir <mushfiq.islam@hungrynaki.com>
2022-12-06 17:29:41 +0600

    [WIP] Dashboard UI

--- app/src/main/java/smir/shitab14/uxperience/dashboard/DashboardActivity.kt
+++ app/src/main/java/smir/shitab14/uxperience/dashboard/DashboardActivity.kt
@@ -1,15 +1,35 @@
 package smir.shitab14.uxperience.dashboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.cardview.widget.CardView
 import smir.shitab14.uxperience.R
+import smir.shitab14.uxperience.tabshow.TabsActivity
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var cvTabTab: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
         supportActionBar?.hide()
 
+        cvTabTab = findViewById<CardView>(R.id.cvTabTab)
+        cvTabTab.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            cvTabTab -> {
+                val intent = Intent(this, TabsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+
 }
---
+++ app/src/main/java/smir/shitab14/uxperience/tabshow/TabsActivity.kt
@@ -0,0 +1,12 @@
+package smir.shitab14.uxperience.tabshow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import smir.shitab14.uxperience.R
+
+class TabsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabs)
+    }
+}
